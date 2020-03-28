1c60d31f96a33ba7d862722992fd09c73fef199a
Costas Basdekis <costas.basdekis@gmail.com>
2020-03-28 13:59:54 +0000

    First build

---
+++ docs/precache-manifest.3150a0408a11dff0f3e9422dce18c0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b2a2654d726d6b0222025ae0739ceb",
+    "url": "/gaiforming-mars/index.html"
+  },
+  {
+    "revision": "cdc76526ccf9aed95a2c",
+    "url": "/gaiforming-mars/static/css/main.bbc15533.chunk.css"
+  },
+  {
+    "revision": "ae41f1358b1e171f01fe",
+    "url": "/gaiforming-mars/static/js/2.c979dcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gaiforming-mars/static/js/2.c979dcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc76526ccf9aed95a2c",
+    "url": "/gaiforming-mars/static/js/main.eb2f04b2.chunk.js"
+  },
+  {
+    "revision": "a37fa869e1543fb2a342",
+    "url": "/gaiforming-mars/static/js/runtime-main.83e39e18.js"
+  }
+]);
