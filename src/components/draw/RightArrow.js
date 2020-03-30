d298e2d85293c12b133b80eb7b413480a5f94913
Costas Basdekis <costas.basdekis@gmail.com>
2020-03-30 01:28:43 +0100

    Update Arrow to standard way

--- src/components/draw/RightArrow.js
+++ src/components/draw/RightArrow.js
@@ -1,20 +1,32 @@
 import React, {Component} from "react";
 
 class RightArrow extends Component {
-  static path = (
-    "M15.375,7L10,2.54C9.695,2.287,9.461,2,9,2C8.375,2,8,2.516,8" +
-    ",3v3H1C0.45,6,0,6.45,0,7v2c0,0.55,0.45,1,1,1h7v3  c0,0.484" +
-    ",0.375,1,1,1c0.461,0,0.695-0.287,1-0.54L15.375,9C15.758" +
-    ",8.688,16,8.445,16,8S15.758,7.313,15.375,7z"
-  );
+  static path = <path d={`
+    M 15.375 12
+    L 10 7.54
+    C 9.695 7.287 9.461 7 9 7
+    C 8.375 7 8 7.516 8 8
+    v 3
+    H 1
+    C 0.45 11 0 11.45 0 12
+    v 2
+    c 0 0.55 0.45 1 1 1
+    h 7
+    v 3
+    c 0 0.484 0.375 1 1 1
+    c 0.461 0 0.695 -0.287 1 -0.54
+    L 15.375 14
+    C 15.758 13.688 16 13.445 16 13
+    S 15.758 12.312999999999999 15.375 12
+    z
+  `} />;
 
   render() {
     const {offset} = this.props;
     return (
-      <path
-        transform={`translate(${offset.x}, ${offset.y - 5})`}
-        d={this.constructor.path}
-      />
+      <g transform={`translate(${offset.x}, ${offset.y})`}>
+        {this.constructor.path}
+      </g>
     );
   }
 }
